9b4f298e6eb1a55decbc81908c80180bfce1217a
wangbertha <wangbertha@gmail.com>
2024-10-02 14:18:26 -0500

    Remove App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
-import './App.css'
 import router from './router'
 
 function App() {
 
   return (
     <RouterProvider router={router} />
-    // <div id="container">
-    //   <h1>Hello React Router!</h1>
-    //   <div id="navbar">{/* navigation here */}</div>
-    //   <div id="main-section">{/* routes here */}</div>
-    // </div>
   )
 }
 
